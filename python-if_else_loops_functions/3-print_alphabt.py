417f1d0125fe44eca411ee75d2a2a8b25ae1f364
Wawa <chutaphat@hotmail.com>
2025-05-07 21:44:59 +1000

    new line at the end

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,4 @@
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'q' and chr(i) != 'e':
         print("{}".format(chr(i)), end="")
-
-# The code prints the letters of the alphabet from 'a' to 'z', excluding 'q' and 'e'.        
+       
