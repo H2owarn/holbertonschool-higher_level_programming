3f7f497eee7b2d79e6bb823a0faa74859bd33928
Wawa <chutaphat@hotmail.com>
2025-05-08 17:10:58 +1000

    fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,8 @@
 for i in range(1,90):
     if i % 10 == 0:
         continue
-    print("{}".format(f"{i:02d}"), end=", ")
+    if i % 11 == 0:
+        continue
+    if i in [21, 31, 41, 51, 61, 71, 81]:
+        continue
+    print("{}".format(f"{i:02d}"), end=", " if i != 89 else "\n")
