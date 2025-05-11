38530ab12452c9d80a2de466467e9dd525db94dd
Wawa <chutaphat@hotmail.com>
2025-05-11 22:47:12 +1000

    stly

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 def main():
-    """Print the number of arguments and their values."""
+
+
     arguments = sys.argv[1:]
     if len(arguments) == 0:
         print(f"{len(arguments)} arguments.")
@@ -11,6 +12,7 @@ def main():
         print(f"{len(arguments)} arguments:")
     for i, arg in enumerate(arguments, start=1):
         print(f"{i}: {arg}")
+
 
 if __name__ == "__main__":
 
