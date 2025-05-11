0db87ed2209f6b5dfa7f17b847d252fc98543179
Wawa <chutaphat@hotmail.com>
2025-05-11 22:44:01 +1000

    s

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,10 +4,10 @@ def main():
     arguments = sys.argv[1:]
     if len(arguments) == 0:
         print(f"{len(arguments)} argument:")
-    elif len(arguments) > 1:
+    elif len(arguments) == 1:
         print(f"{len(arguments)} argument:")
     else:
-        print(f"{len(arguments)} argument:")
+        print(f"{len(arguments)} arguments:")
     for i, arg in enumerate(arguments, start=1):
         print(f"{i}: {arg}")
 
