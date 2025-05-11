f3b926a5f8cb539d14666dcf9b160393c1f79332
Wawa <chutaphat@hotmail.com>
2025-05-11 22:53:53 +1000

    permission

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-def main():
-    import sys
+import sys
 
+
+def main():
+    
     numbers = sys.argv[1:]
     if len(numbers) == 0:
         print("0")
     else:
         total = sum(int(num) for num in numbers)
         print(total)
+
 
 if __name__ == "__main__":
     main()
