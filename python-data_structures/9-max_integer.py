59cafd13a4503f90927c5a6f18c49c561cbe95f8
Wawa <chutaphat@hotmail.com>
2025-05-14 00:04:17 +1000

    max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    
+    max_value = my_list[0]
     if len(my_list) == 0:
         return None
     
-    max_value = my_list[0]
+
     for i in my_list:
         if i > max_value:
             max_value = i
-    return(max_value)
+    return (max_value)
