6684747ce075f7514af4e0b1f1e433236eb3979b
Wawa <chutaphat@hotmail.com>
2025-05-15 14:19:22 +1000

    style

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sort_dic = dict(sorted(a_dictionary.items()))
-    for key , value in sort_dic.items():
-        print (f"{key}: {value}")
+    for key, value in sort_dic.items():
+        print(f"{key}: {value}")
