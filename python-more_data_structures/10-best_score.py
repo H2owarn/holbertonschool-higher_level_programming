ef1cb5eafc10e8a1fe70be057eddf519fae0e162
Wawa <chutaphat@hotmail.com>
2025-05-16 12:22:57 +1000

    style

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     best_key = None
-    best_s = float('-inf') #smallest posible number
+    best_s = float('-inf') ## smallest posible number
  
     for key, value in a_dictionary.items():
         if value > best_s:
