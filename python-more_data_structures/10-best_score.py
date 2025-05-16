731204aada34f4d5ba20b06ff94affee1ea4deb9
Wawa <chutaphat@hotmail.com>
2025-05-16 12:21:44 +1000

    float('-inf') #smallest posible number

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_s = float('-inf') #smallest posible number
+ 
+    for key, value in a_dictionary.items():
+        if value > best_s:
+            best_s = value
+            best_key = key
+    return (best_key)
