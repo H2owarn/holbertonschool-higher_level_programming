94e3ad463b560f35abeb761fa588977a6f51e5b3
Wawa <chutaphat@hotmail.com>
2025-05-20 20:56:13 +1000

    test5

--- python-test_driven_development/.py
+++ python-test_driven_development/.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 text_indentation = __import__('5-text_indentation').text_indentation
 
-text_indentation("Holberton School")
+text_indentation("Let's print a new-line! Here goes:\nPrinted.")
  
