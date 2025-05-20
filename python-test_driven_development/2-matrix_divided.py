bbc61e3b53e41dd7a7aa609bff27d3b8c85ad0ba
Wawa <chutaphat@hotmail.com>
2025-05-20 11:37:52 +1000

    test1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -17,23 +17,27 @@ def matrix_divided(matrix, div):
         TypeError: If div is not a number.
         ZeroDivisionError: If div is zero.
     """
+    result = []
 
-    # Validate matrix is a list of lists of numbers
-    if (not isinstance(matrix, list) or
-            not all(isinstance(row, list) for row in matrix) or
-            not all(isinstance(num, (int, float)) for row in matrix for num in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix"
+                        " (list of lists) of integers/floats")
+    if not all(isinstance(i, (int, float)) for row in matrix for i in row):
+        raise TypeError("matrix must be a matrix"
+                        " (list of lists) of integers/floats")
 
-    # Validate all rows are the same length
-    row_length = len(matrix[0])
-    if not all(len(row) == row_length for row in matrix):
+    if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Validate div
-    if not isinstance(div, (int, float)):
+    if type(div) not in (float, int):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    # Return new matrix
-    return [[round(num / div, 2) for num in row] for row in matrix]
+
+    for row in matrix:
+        new_row = []
+        for i in row:
+            new_row.append(round(i/div, 2))
+        result.append(new_row)
+    return result
