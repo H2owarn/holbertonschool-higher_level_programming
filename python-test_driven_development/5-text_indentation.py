0b2691181cd06e7e1184868a73ec58b585b31d26
Wawa <chutaphat@hotmail.com>
2025-05-20 19:38:23 +1000

    fixed

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,6 +16,15 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError ("text must be a string")
     
-    formatted_text = text.replace(",", ",\n\n").replace("?", "?\n\n").replace(":", ":\n\n").replace(".", ".\n\n")
-    
-    print(formatted_text.strip(), end="")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+
+        if text[i] in ['.', '?', ':', ',']:
+            print("\n")
+            i += 1
+
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
