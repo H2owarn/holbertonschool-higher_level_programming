e14d169dc8027be09fe3ff2a2ff0679bda3f4297
Wawa <chutaphat@hotmail.com>
2025-05-20 16:34:27 +1000

    test5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,4 +17,4 @@ def text_indentation(text):
         raise TypeError ("text must be a string")
     
     formatted_text = text.replace(",", ",\n\n").replace("?", "?\n\n").replace(":", ":\n\n").replace(".", ".\n\n")
-    print(formatted_text, end="")
+    print(formatted_text.strip(), end="")
