0b401aef73b5a7a14a2f8104e7860b59435bd0a0
Wawa <chutaphat@hotmail.com>
2025-05-20 11:06:46 +1000

    test2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,20 +2,20 @@
 """This module provides a function to print a formatted name."""
 
 
-def say_my_name(first_name, last_name):
+def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is <first name> <last name>"
-
-    Args:
-        first_name: str
-        last_name: str
+    Prints 'My name is <first_name> <last_name>'
+    
+    Arguments:
+    first_name (str): The first name to print
+    last_name (str): The last name to print (optional)
 
     Raises:
-        TypeError: if either name is not a string
-
+    TypeError: If first_name or last_name is not a string
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {}{}".format(first_name, f" {last_name}" if last_name else ""))
+
+    print("My name is {} {}".format(first_name, last_name))
