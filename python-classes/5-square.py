27fa480c4e42631e2a5008983e67b5c013198732
Wawa <chutaphat@hotmail.com>
2025-05-21 20:30:28 +1000

    print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a Square class with size validation and area calculation."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size   # private class
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Return the square withe the character #"""
+        if self.__size == 0:
+            print("")  # print emty line
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
