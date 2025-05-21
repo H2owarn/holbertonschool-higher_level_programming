784b5708b637e7ce96b3cb1cd4bdc3cb8b79c2c0
Wawa <chutaphat@hotmail.com>
2025-05-21 12:28:13 +1000

    style

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square with a private size attribute."""
 
-
     def __init__(self, size):
 
-        self.__size = size # Private instance attribute
+        self.__size = size """Private instance attribute"""
+
