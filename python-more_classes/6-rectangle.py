164640b776184b34432b3d04b06e414158db9fd8
Wawa <chutaphat@hotmail.com>
2025-05-25 17:09:54 +1000

    number_of_instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,9 +4,12 @@
 
 class Rectangle:
     """Represents a rectangle with a private size attribute."""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -59,4 +62,5 @@ class Rectangle:
 
     def __del__(self):
         """Print message when the object is deleted"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
