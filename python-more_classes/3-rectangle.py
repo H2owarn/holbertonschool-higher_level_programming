379470c715a2e61d6452af9ea3e80b67a24def41
Wawa <chutaphat@hotmail.com>
2025-05-25 16:18:53 +1000

    style

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,9 +45,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """Return the rectangel."""
         if self.__width <= 0 or self.__height <= 0:
             return ""
 
-        return "\n".join(["#" * self.__width] * self.__height) 
+        return "\n".join(["#" * self.__width] * self.__height)
