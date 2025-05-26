eab8547c808ecabfde34f30b73589b444b9967f8
Wawa <chutaphat@hotmail.com>
2025-05-26 14:17:49 +1000

    Mylist inherits list and test

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module write a class MyList that inherits from list."""
+
+
+class MyList(list):
+    """Return class MyList that inherits from list."""
+    def print_sorted(self):
+        print (self)
