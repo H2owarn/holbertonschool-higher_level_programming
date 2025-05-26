c903ccd583391c4cd7993117fa6744f2aeff9862
Wawa <chutaphat@hotmail.com>
2025-05-26 15:11:32 +1000

    style

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module write a function that returns True if the object
-    is exactly an instance of the specified class ; otherwise False."""  
+    is exactly an instance of the specified class ; otherwise False."""
 
 
 def is_same_class(obj, a_class):
-        """Return if obj is an instance of a_class"""
-        return type(obj) is a_class
+    """Return if obj is an instance of a_class"""
+    return type(obj) is a_class
