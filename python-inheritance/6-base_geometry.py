560c5d6199a17efd80efb7dcfbb14078aadc472d
Wawa <chutaphat@hotmail.com>
2025-05-27 12:54:11 +1000

    raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module write Write a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class for geometric operations."""
+    
+    def area(self):
+        """Raises an exception since area() is not implemented."""
+        raise Exception("area() is not implemented")
