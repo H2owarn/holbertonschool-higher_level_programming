953e7a5b5303107f225161d37581e6db4a1abcab
Wawa <chutaphat@hotmail.com>
2025-05-27 12:15:27 +1000

    check if subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,5 +8,5 @@ from the class itself.
 
 def inherits_from(obj, a_class):
     """Return True if the object is an instance of a class that inherited 
-    (directly or indirectly) from the specified class"""
+    (directly or indirectly)"""
     return type(obj) is not a_class and issubclass(type(obj), a_class)
