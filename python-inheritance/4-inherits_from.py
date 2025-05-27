6a5254fd2dcbe40666b2206d65c5e37940372ebc
Wawa <chutaphat@hotmail.com>
2025-05-27 12:17:40 +1000

    style

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module write function check if the object’s class 
+"""This module write function check if the object’s class
 is a subclass"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if the object is an instance of a class that inherited 
+    """Return True if the object is an instance of a class that inherited
     (directly or indirectly)"""
     return type(obj) is not a_class and issubclass(type(obj), a_class)
