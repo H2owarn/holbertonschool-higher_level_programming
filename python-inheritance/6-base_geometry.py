964393afa49ae56c863052fb215784be8d21c22f
Wawa <chutaphat@hotmail.com>
2025-05-27 12:54:57 +1000

    whitespace

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry:
     """A class for geometric operations."""
-    
+
     def area(self):
         """Raises an exception since area() is not implemented."""
         raise Exception("area() is not implemented")
