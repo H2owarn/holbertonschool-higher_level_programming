9566916831e1252a5bf7b23e2b53ca8e933afd72
Wawa <chutaphat@hotmail.com>
2025-05-27 15:44:21 +1000

    import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """This module write Write a class BaseGeometry and Rectangle classes"""
 
-class BaseGeometry:
-    """A class for geometric operations."""
-
-    def area(self):
-        """Raises an exception since area() is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates if the given value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """A class for Rectangle"""
