96a19aeaab52985ae33c2bd8dc4831092b01ca7b
Wawa <chutaphat@hotmail.com>
2025-05-28 14:06:27 +1000

    math and blueprint

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""This module create an abstract class named Shape
+ with two abstract methods: area and perimeter."""
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+ # Subclass implementing the blueprint
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius ** 2
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(Shape):
+    print ("Area: {}".format(Shape.area()))
+    print ("Perimeter:: {}".format(Shape.perimeter()))
