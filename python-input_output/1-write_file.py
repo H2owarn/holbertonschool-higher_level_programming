d83a442c5109381b534d49ddc03687e114da8aab
Wawa <chutaphat@hotmail.com>
2025-06-05 12:45:39 +1000

    w

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module writr the function that write content in to the file"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding="utf-8") as file:
+        write_content = file.write(text)
+        return write_content
