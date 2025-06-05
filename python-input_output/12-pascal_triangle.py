5b5cf93366cbae1dee5881ac94fcb39a004d69de
Wawa <chutaphat@hotmail.com>
2025-06-05 17:16:03 +1000

    style

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,7 +12,6 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]  # first element is always 1
 
-
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
