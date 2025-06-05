ff31f7cdfbbb58d3fe8a5fb1a30148ecfa2121de
Wawa <chutaphat@hotmail.com>
2025-06-05 13:07:20 +1000

    style

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module write a function append strint to text file """
 
+
 def append_write(filename="", text=""):
     """append a string to the end of a text file"""
     with open(filename, 'a', encoding="utf-8") as file:
