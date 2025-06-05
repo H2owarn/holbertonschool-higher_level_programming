185700c57d45623d1efe3da7d790b8bbee19f04e
Wawa <chutaphat@hotmail.com>
2025-06-05 14:37:42 +1000

    object to text

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module writes an Object to a text file,
+ using a JSON representation"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using a JSON"""
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
