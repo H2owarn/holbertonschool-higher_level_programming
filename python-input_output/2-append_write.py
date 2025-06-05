4b49154ca9b82ac5edd1e4af65b43adb797fefe3
Wawa <chutaphat@hotmail.com>
2025-06-05 13:05:33 +1000

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module write a function append strint to text file """
+
+def append_write(filename="", text=""):
+    """append a string to the end of a text file"""
+    with open(filename, 'a', encoding="utf-8") as file:
+        append_lenth = file.write(text)
+        return append_lenth
