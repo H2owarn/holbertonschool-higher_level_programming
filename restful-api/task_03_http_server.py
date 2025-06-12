cba01126a672e49b26a88d0d9058e987bb6350e0
Wawa <chutaphat@hotmail.com>
2025-06-13 02:14:25 +1000

    fix:Endpoint not found

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,7 +31,7 @@ class MyHandler(BaseHTTPRequestHandler):
              self.send_response(404)
              self.send_header("Content-type", "text/plain")
              self.end_headers()
-             self.wfile.write(b"404 Not Found")
+             self.wfile.write(b"Endpoint not found")
 
     #  start the web server
 
