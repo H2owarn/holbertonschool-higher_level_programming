03135103323811f2ba5271f50ce28a0f58285a14
Wawa <chutaphat@hotmail.com>
2025-06-14 01:17:37 +1000

    Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+#  When someone visits the / endpoint
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+users = {
+    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"name": "John", "age": 30, "city": "New York"}
+}
+
+@app.route("/data")
+def username():
+    return jsonify(list(users.keys()))
+
+@app.route("/users/<username>")
+def get_user(username):
+    if username in users:
+        return jsonify(users[username])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+
+    data = request.get_json()
+
+    username = data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+    return jsonify({"message": "User added!", "user": users[username]}), 201
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
