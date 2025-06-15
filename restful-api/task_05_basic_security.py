fd2c4c655fc3e05f4f5b2d1f17e83c82487badd6
Wawa <chutaphat@hotmail.com>
2025-06-15 22:21:38 +1000

    fix:user data

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -26,7 +26,13 @@ jwt = JWTManager(app)
 
 
 # ─────────── User store ────────
-users = {}
+users = {
+
+    "user1": {"username": "user1", "password": generate_password_hash("password"),
+                "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"),
+                "role": "admin"}
+    }
 
 # ─────────── User store ────────
 auth = HTTPBasicAuth()
