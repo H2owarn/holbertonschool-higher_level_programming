24eb8ebccf745a604bd03568aca7c6c66282ecb7
Wawa <chutaphat@hotmail.com>
2025-06-24 15:50:01 +1000

    creat the table and ad values

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates full table in the database
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    score INT NOT NULL,
+    PRIMARY KEY (id)
+)
+
+-- Inserts a new row in the table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
