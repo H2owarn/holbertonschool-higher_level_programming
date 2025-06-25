5eb9c6a42c8b85103f7641aa6878f939a7855c18
Wawa <chutaphat@hotmail.com>
2025-06-25 22:18:40 +1000

    fix: chang attribute name

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 --  lists the number of records with the same score (counts how many times each score appears)
-SELECT score, COUNT(*) AS count
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
