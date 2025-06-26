80fcd1fd4ffb87e00e0c87bff9445bac7c4e82e0
Wawa <chutaphat@hotmail.com>
2025-06-26 19:25:50 +1000

    wrong word

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 
 SELECT tv_show.title, tv_show_genre.genre_id
 FROM tv_shows
-JOINT tv_show_genre ON tv_shows.id = tv_show_genre.tv_show_id
+JOIN tv_show_genre ON tv_shows.id = tv_show_genre.tv_show_id
 ORDER BY tv_show.title, tv_show_genre.genre_id;
