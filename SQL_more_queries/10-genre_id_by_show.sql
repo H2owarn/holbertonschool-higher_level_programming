764bc61c7f68fa1e92d6a2b9a644f47bec2c70bf
Wawa <chutaphat@hotmail.com>
2025-06-26 19:28:12 +1000

    fix worng table name

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 --  lists all shows contained in hbtn_0d_tvshows
 
 
-SELECT tv_show.title, tv_show_genre.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genre ON tv_shows.id = tv_show_genre.tv_show_id
-ORDER BY tv_show.title, tv_show_genre.genre_id;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
