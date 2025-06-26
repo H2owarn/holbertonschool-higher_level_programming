4428d75c9a4ce024d37fd680201e7f7e04797ef8
Wawa <chutaphat@hotmail.com>
2025-06-26 11:07:41 +1000

    creat user

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,13 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+USE hbtn_0c_0;
 
 -- convert database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Convert table
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert th efield 'name'
+
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
