9e8584768b20370274ab21798e7ebe55b7a01a67
Wawa <chutaphat@hotmail.com>
2025-06-26 19:24:07 +1000

    list the show by joint table

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+--  lists all shows contained in hbtn_0d_tvshows
+
+
+SELECT tv_show.title, tv_show_genre.genre_id
+FROM tv_shows
+JOINT tv_show_genre ON tv_shows.id = tv_show_genre.tv_show_id
+ORDER BY tv_show.title, tv_show_genre.genre_id;
