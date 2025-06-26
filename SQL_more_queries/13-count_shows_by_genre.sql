58d9313ee8854daa6db17be3599f8b047eb466c7
Wawa <chutaphat@hotmail.com>
2025-06-26 20:19:12 +1000

    list 2 join tables

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+--  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+
+SELECT tv_genres.name as genre, COUNT(tv_shows.id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
