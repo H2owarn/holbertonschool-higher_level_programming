c6d1c2fa32c95e6a7ed4c4ff90be7ea1db92b2bb
Wawa <chutaphat@hotmail.com>
2025-06-30 13:23:47 +1000

    stly fixed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,12 +11,11 @@ import sys
 
 if __name__ == "__main__":
 
-
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
 
-    db = MySQLdb.connect (
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
