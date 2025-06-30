7348635e42f6d0ae3add6f68a4595353acdc5f3d
Wawa <chutaphat@hotmail.com>
2025-06-30 13:29:26 +1000

    list all the states start with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""This script connects to a MySQL database and retrieves
+all states from the 'states' table whose names start with 'N'."""
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # fetch and print all rows
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+
