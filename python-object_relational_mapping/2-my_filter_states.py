c96df6249a13731629c5bb4f187559ab99ab83e8
Wawa <chutaphat@hotmail.com>
2025-06-30 14:19:01 +1000

    fix code style

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     dbname = sys.argv[3]
     state_name = sys.argv[4]
 
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,10 +22,12 @@ if __name__ == "__main__":
         db=dbname)
 
     cur = db.cursor()
-    query = "SELECT * " \
-    "FROM states " \
-    "WHERE name = '{}' " \
-    "ORDER BY id ASC".format(state_name)
+    query = (
+        "SELECT * "
+        "FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC"
+    ).format(state_name)
 
     cur.execute(query)
 
