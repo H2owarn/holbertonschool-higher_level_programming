375d81f4a23c647092fd09ba6ca0bbf3845cc36b
Wawa <chutaphat@hotmail.com>
2025-07-01 14:32:47 +1000

    list al the state by SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This script connects to a MySQL database and retrieves
+all State objects from the 'states' table, sorted by id."""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+# Connect to MySQL using SQLAlchemy engine
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{dbname}',
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+# Create a session to interact with the DB
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Query all State objects, sorted by id
+
+states = session.query(State).order_by(State.id).all()
+
+#  Print the results
+
+for state in states:
+    print(f"{state.id}: {state.name}")
+
+session.close()
