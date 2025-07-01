67b302c6c5c82b4d015f2ae7a797cbd96985c2c1
Wawa <chutaphat@hotmail.com>
2025-07-01 15:47:40 +1000

    prints the State object with the name passed

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This script connects to a MySQL database and retrieves
+all State objects from the 'states' table that contain the letter 'a'."""
+
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL using SQLAlchemy engine
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{dbname}',
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the DB
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the State with the name passed as argument
+    states = (
+        session.query(State)
+        .filter(State.name == state_name)
+        .order_by(State.id)
+        .all()
+    )
+
+    # Print the result
+    for state in states:
+        print(f"{state.id}")
+
+    session.close()
