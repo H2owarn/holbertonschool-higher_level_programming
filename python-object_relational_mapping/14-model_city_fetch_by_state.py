07bc99b201b29dd97980a8bf0c62f9aa83412d1a
Wawa <chutaphat@hotmail.com>
2025-07-02 19:52:49 +1000

    Create city table and join with state

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""This script connects to a MySQL database and retrieves
+all cities from the 'cities' table along with their states."""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # Connect to MySQL using SQLAlchemy engine
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{dbname}',
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the DB
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Delete all State objects with the name 'a'
+    cities = (
+        session.query(State.name, City.id, City.name)
+        .join(State, City.state_id == State.id)
+        .all()
+    )
+
+    for city in cities:
+        print(f"{city[0]}: ({city[1]})  {city[2]}")
+
+
+
+    session.close()
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module defines the City class for use with SQLAlchemy ORM."""
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class City(Base):
+    """City class for SQLAlchemy ORM."""
+    __tablename__ = 'cities'
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+        unique=True
+    )
+    name = Column(String(128), nullable=False)
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
