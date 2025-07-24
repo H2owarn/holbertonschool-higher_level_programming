37b31ec743db321743f7e4f39bdc0f0d3f1fa25f
Wawa <chutaphat@hotmail.com>
2025-07-24 11:00:00 +1000

    use a counter during looping instead of length

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
+let count = 0;
 
-if (args.length === 0) {
+args.forEach(() => {
+  count += 1;
+});
+
+if (count === 0) {
   console.log('No argument');
 } else {
   console.log(args[0]);
