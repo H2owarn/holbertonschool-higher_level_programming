1f6b64afce4a11334dff147ff4e81dbb1b70d028
Wawa <chutaphat@hotmail.com>
2025-07-24 13:42:44 +1000

    Base on task 5 add repeat

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const isInteger = /^[+-]?\d+$/.test(args);
+
+if (isInteger) {
+  const num = parseInt(args, 10);
+  let i = 0;
+  while (i < num) {
+    console.log('x'.repeat(num));
+    i += 1;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
